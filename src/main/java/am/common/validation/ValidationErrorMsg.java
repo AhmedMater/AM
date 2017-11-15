5f5c49e27d8a44e16bc9607c91f0ff688449b303
AhmedMater <ahmedmotair@gmail.com>
2017-11-16 01:32:28 +0200

    Implement the Form Validation in the Backend Implement Course Details

---
+++ src/main/java/am/common/validation/ValidationErrorMsg.java
@@ -0,0 +1,33 @@
+package am.common.validation;
+
+/**
+ * Created by ahmed.motair on 11/16/2017.
+ */
+public class ValidationErrorMsg {
+    public static final String AMT_VALID_001_ST = "AM-VALID-001: Field '";
+    public static final String AMT_VALID_001_EN = "' is mandatory";
+
+    public static final String AMT_VALID_002_ST = "AM-VALID-002: Length of Field '";
+    public static final String AMT_VALID_002_EN = "' has to be within";
+
+    public static final String AMT_VALID_003_ST = "AM-VALID-003: Invalid Field '";
+    public static final String AMT_VALID_003_EN = "' as It allows only ";
+
+    public static final String AMT_VALID_004_ST = "AM-VALID-004: Field '";
+    public static final String AMT_VALID_004_EN = "' can't be empty";
+
+    public static final String AMT_VALID_005_ST = "AM-VALID-005: Length of Field '";
+    public static final String AMT_VALID_005_EN = "' has to be of Min ";
+
+    public static final String AMT_VALID_006_ST = "AM-VALID-006: Length of Field '";
+    public static final String AMT_VALID_006_EN = "' has to be of Max";
+
+    public static final String AMT_VALID_007_ST = "AM-VALID-007: Length of Field '";
+    public static final String AMT_VALID_007_EN = "' has to be of ";
+
+    public static final String REAL_NAME_REGEX_MSG = "chars, hyphen, comma, period, and Apostrophe";
+    public static final String CONTENT_NAME_REGEX_MSG = "chars, hyphen, comma, period, space and Apostrophe";
+    public static final String USERNAME_REGEX_MSG = "chars, numbers, period, hyphen, and Underscore";
+    public static final String PASSWORD_REGEX_MSG = "chars, numbers, period, hyphen, Ampersand, and Underscore";
+    public static final String POSITIVE_NUM_REGEX_MSG = "positive numbers";
+}
